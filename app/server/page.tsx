37fb8980e29ee03ae335d23a899725109b39e261
fcaramez <francisco.caramez.duarte@gmail.com>
2025-09-13 01:29:07 +0100

    hook running fine

--- app/server/page.tsx
+++ app/server/page.tsx
@@ -3,11 +3,6 @@ export default async function ServerPage() {
     <main className="mx-auto flex max-w-2xl flex-col gap-4 p-8">
       <h1 className="text-center text-4xl font-bold">Convex + Next.js</h1>
       <div className="flex flex-col gap-4 rounded-md bg-slate-200 p-4 dark:bg-slate-800">
-        <button
-          onClick={() => {
-            return '';
-          }}
-        ></button>
         <h2 className="text-xl font-bold">Non-reactive server-loaded data</h2>
       </div>
     </main>
